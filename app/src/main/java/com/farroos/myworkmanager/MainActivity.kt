9b0b3ad4d548e78b7e79e877be686ffd73753fcf
muhamad farroos <muhammadfarroos12@gmail.com>
2021-10-14 00:54:25 +0700

    first commit

---
+++ app/src/main/java/com/farroos/myworkmanager/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.farroos.myworkmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.work.*
+import com.farroos.myworkmanager.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var workManager: WorkManager
+
+    private lateinit var periodicWorkRequest: PeriodicWorkRequest
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        workManager = WorkManager.getInstance(this)
+        binding.btnOneTimeTask.setOnClickListener (this)
+        binding.btnPeriodicTask.setOnClickListener(this)
+        binding.btnCancelTask.setOnClickListener(this
+        )
+
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btnOneTimeTask -> startOneTimeTask()
+            R.id.btnPeriodicTask -> startPeriodicTask()
+            R.id.btnCancelTask -> startCancelTask()
+        }
+    }
+
+    private fun startOneTimeTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val onTimeWorkRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+            .setInputData(data)
+            .build()
+        workManager.enqueue(onTimeWorkRequest)
+        workManager.getWorkInfoByIdLiveData(onTimeWorkRequest.id)
+            .observe(this@MainActivity, { workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+            })
+    }
+
+
+    private fun startCancelTask() {
+        workManager.cancelWorkById(periodicWorkRequest.id)
+    }
+
+    private fun startPeriodicTask() {
+        binding.textStatus.text = getString(R.string.status)
+        val data = Data.Builder()
+            .putString(MyWorker.EXTRA_CITY, binding.editCity.text.toString())
+            .build()
+        val constrains = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        periodicWorkRequest = PeriodicWorkRequest.Builder(MyWorker::class.java, 15, TimeUnit.MINUTES)
+            .setInputData(data)
+            .setConstraints(constrains)
+            .build()
+        workManager.enqueue(periodicWorkRequest)
+        workManager.getWorkInfoByIdLiveData(periodicWorkRequest.id)
+            .observe(this@MainActivity, {workInfo ->
+                val status = workInfo.state.name
+                binding.textStatus.append("\n" + status)
+                binding.btnCancelTask.isEnabled = false
+                if (workInfo.state == WorkInfo.State.ENQUEUED){
+                    binding.btnCancelTask.isEnabled = true
+                }
+            })
+    }
+
+}
---
+++ app/src/main/java/com/farroos/myworkmanager/Response.kt
@@ -0,0 +1,21 @@
+package com.farroos.myworkmanager
+
+import com.squareup.moshi.Json
+
+data class Response(
+    val id: Int,
+    val name: String,
+    @Json(name = "Weather")
+    val weatherList: List<Weather>,
+    val main: Main
+)
+
+data class Weather(
+    val main: String,
+    val description: String
+)
+
+data class Main(
+    @Json(name = "temp")
+    val temperature: Double
+)
